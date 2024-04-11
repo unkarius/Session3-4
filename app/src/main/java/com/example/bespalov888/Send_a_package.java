f364f7013ef48d8a78bb87f578b4a529a7d88392
BOBRO <die.darkholme@yandex.ru>
2024-04-12 01:45:43 +0300

    wassap

---
+++ app/src/main/java/com/example/bespalov888/Send_a_package.java
@@ -0,0 +1,14 @@
+package com.example.bespalov888;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Send_a_package extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_apackage);
+    }
+}
